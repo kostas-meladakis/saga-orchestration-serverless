f8575bcde984e37223815509f634cc933b37764b
kostas-meladakis <57420533+kostas-meladakis@users.noreply.github.com>
2022-01-12 00:40:38 +0200

    Update backend.tf

--- iac/backend.tf
+++ iac/backend.tf
@@ -3,6 +3,5 @@ terraform {
     # modify the values for the storage account
     storage_account_name = "azurestoragebymela"
     container_name       = "testingterraform"
-    key                  = "key1"
   }
 }
